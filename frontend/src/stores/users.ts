714d0f1666e4c1abc631b5c3fc4ace7759c64cc3
Noah Westerhold <nwesterhold@techfak.uni-bielefeld.de>
2025-04-24 14:10:37 +0200

    Backend Calls wurden (auskommentiert) in der Frontend Komponente eingebaut refs #93480 @0:40

--- frontend/src/stores/users.ts
+++ frontend/src/stores/users.ts
@@ -1,4 +1,6 @@
 import {defineStore} from 'pinia'
+import type Credentials from "@/types/Credentials";
+import api from "@/api";
 
 export const useUserStore = defineStore('users', {
   state: () => ({
@@ -10,7 +12,46 @@ export const useUserStore = defineStore('users', {
       {email: 'test@test.com', password: '123456'},
     ]
   }),
+  /*
+  actions: {
+    authenticate(taken: string|null) : void {
+      if (token != null) {
+        this.authenticated = true;
+        this.email = email;
+        let j;
+        for (j = 0; j < email.length; j++) {
+            if (email[j] === '@') {
+              this.username = email.slice(0, j);
+              break;
+            }
+          }
+        localStorage.setItem('token',token);
+      } else {
+        this.authenticated = false;
+        }
+      },
 
+      logout() : void {
+      this.authenticated = false;
+      this.username = '';
+      this.email = '';
+      localStorage.removeItem('token');
+      }
+
+      // email taken , addUser müssen noch gemacht werden
+      requestToken(credentials: Credentials) : Promise<void> {
+         return new Promise<void>((resolve, reject) => {
+          api.authorization.login(credentials.username, credentials.password).then((res) => {
+            this.authenticate(res.headers.authorization);
+            resolve();
+          }).catch(() => {
+            this.authenticate(null);
+            reject();
+           })
+          })
+         }
+       }
+   */
   actions: {
     authenticate(email: string, password: string) {
       let i;
