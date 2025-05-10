f9adca757bacb71d88e344fe79ca344b0f2a61b0
Lara <lkuelker@techfak.uni-bielefeld.de>
2025-05-10 12:15:36 +0200

    Dashboard Components zu Main hinzugefügt

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -7,7 +7,10 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Adder: typeof import('./components/Adder.vue')['default']
     AppFooter: typeof import('./components/AppFooter.vue')['default']
+    Dashboard: typeof import('./components/Dashboard.vue')['default']
+    Decks: typeof import('./components/Decks.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     LoginForm: typeof import('./components/LoginForm.vue')['default']
     Profile: typeof import('./components/Profile.vue')['default']
