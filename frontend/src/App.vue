6f2be820a045fc81b86b64db0c94ba426feaf7bc
Noah Westerhold <nwesterhold@techfak.uni-bielefeld.de>
2025-05-21 14:01:08 +0200

    Wenn ein nutzer nicht angemeldet ist wird beim neuladen der seite das standartdahboard erstellt und der nutzer abgemeldet

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,17 @@
 </template>
 
 <script setup lang="ts">
+import {onMounted} from 'vue';
+import {useUserStore} from "@/stores/users"
+import {useDeckStore} from "@/stores/deck";
 
+const deckStore = useDeckStore()
+const userStore = useUserStore()
+
+onMounted(() => {
+  if(!userStore.authenticated){
+    userStore.logout()
+    deckStore.reset_decks()
+  }
+})
 </script>
