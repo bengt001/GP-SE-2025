89d5872251a9308e53c52ccb915e18b41f35c66d
Lara <lkuelker@techfak.uni-bielefeld.de>
2025-05-24 18:53:01 +0200

    Suchbar erstellt refs #93864 @0:50

---
+++ frontend/src/components/Searchbar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-card
+    class="mx-auto"
+    width="500"
+    border
+    rounded="pill"
+    flat
+  >
+    <template #text>
+      <div class="px-4">
+        <v-text-field
+          density="compact"
+          placeholder="Suchen"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo"
+          clearable
+          flat
+          hide-details
+          single-line
+        />
+      </div>
+    </template>
+  </v-card>
+</template>
