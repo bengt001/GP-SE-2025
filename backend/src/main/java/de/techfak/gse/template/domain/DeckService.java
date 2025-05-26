ce642b3f337f26008088facfe6b2e507f4cd5773
Fabian Friedrich <fafriedrich@techfak.uni-bielefeld.de>
2025-05-26 13:11:36 +0200

    added DeckService Interface from API

--- backend/src/main/java/de/techfak/gse/template/domain/DeckService.java
+++ backend/src/main/java/de/techfak/gse/template/domain/DeckService.java
@@ -12,4 +12,26 @@ public interface DeckService {
     Deck addDeck(Boolean visibility, List<String> fieldOfLaw);
     Deck updateDeck(String id, Boolean visibility, List<String> fieldOfLaw);
 
+    List<Deck> getAllDecks();
+
+    Optional<Deck> getDeckById(long id);
+
+    List<Deck> getUserDecks(Usr usr);
+
+    Optional<Deck> getUserDeckById(Usr usr, long id);
+
+    List<Card> getCards(long deckId);
+
+    Optional<Card> getCardById(long deckId,long id);
+
+    List<Card> getUserCards(Usr usr, long deckId);
+
+    Optional<Card> updateCard(Usr usr, long deckId, long cardId, Card updatedCard);
+
+    Optional<Card> getUseCardById(Usr usr, long deckId,long id);
+
+    Optional<Deck> getNewUserDeck(Usr usr, long templateDeckId);
+
+    Optional<Deck> updateDeck(Usr usr, long deckId, Deck updatedDeck);
+
 }
