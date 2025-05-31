f7b3c75af2fb5518526dd1dd781ac5081aa78006
Noah Westerhold <nwesterhold@techfak.uni-bielefeld.de>
2025-05-31 13:19:42 +0200

    repositorys kopiert um verhalten zu nutzen refs #93960 @0:05

---
+++ backend/src/main/java/de/techfak/gse/template/domain/CardRepository.java
@@ -0,0 +1,21 @@
+package de.techfak.gse.template.domain;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
+/**
+
+ * Repository for cards.
+
+ */
+
+public interface CardRepository extends CrudRepository<Card, Long> {
+
+    @Query("SELECT c FROM Card c JOIN c.deck d WHERE d.deckId = :deckId AND c.cardId = :cardId")
+
+    Optional<Card> findCardByIdAndDeckId(@Param("cardId") long cardId, @Param("deckId") long deckId);
+
+}
+
