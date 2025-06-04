d4ab587bf13d467372bd5a49a3dcaa7cf5bc004b
Lara <lkuelker@techfak.uni-bielefeld.de>
2025-06-04 16:15:50 +0200

    Searchbar passt sich an Bildschirmgröße an

--- frontend/src/components/Searchbar.vue
+++ frontend/src/components/Searchbar.vue
@@ -16,8 +16,34 @@ export default {
 
 <template>
   <v-card
+    v-if="$vuetify.display.mdAndUp"
     class="mx-auto"
     width="500"
+    border
+    rounded="pill"
+    flat
+  >
+    <template #text>
+      <div class="px-4">
+        <v-text-field
+          v-model="searchValue"
+          density="compact"
+          placeholder="Suchen"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo"
+          clearable
+          flat
+          hide-details
+          single-line
+          @input="changeValue"
+        />
+      </div>
+    </template>
+  </v-card>
+  <v-card
+    v-else
+    class="mx-auto"
+    width="300"
     border
     rounded="pill"
     flat
