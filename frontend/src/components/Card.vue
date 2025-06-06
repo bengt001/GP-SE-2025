97b5b212c379d43362f99cfea7728b2605e301f9
Noah Westerhold <nwesterhold@techfak.uni-bielefeld.de>
2025-06-06 20:18:30 +0200

    Im lernmodus sieht man oben wie die letzte Karte bewertet wurde refs #94037 @2:00

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -2,31 +2,45 @@
 import {useCardStore} from "@/stores/card";
 import {useRoute} from "vue-router";
 import {ref} from 'vue'
+import { useTheme } from 'vuetify'
 
-const DialogEnd = ref(false)
-
+const theme = useTheme()
+const colorNames = ['green', 'yellow', 'orange', 'red', 'grey'];
 const cardStore = useCardStore()
 const router = useRouter();
 const route = useRoute<'/cards/[id]/'>()
 const id = route.params.id
+const DialogEnd = ref(false)
 const card = ref(cardStore.findCardById(parseInt(id)))
 const reveal = ref(false)
+const ratingArr = ref<number[]>([])
+const backPossible = ref(false)
+
+const cards = computed(() => cardStore.getCards())
+const curCardIndex = computed(() => cardStore.getCardIndex())
+
+watch(cards, newCards => {
+    ratingArr.value = Array(newCards.length).fill(4);
+  },
+  { immediate: true }
+);
+
 watch (() => route.params.id, (newId) => {
   card.value = cardStore.findCardById(parseInt(newId))
 })
 
-//Buttons letzte Karte, Antwort Zeigen, Home
-const goBack = () => {
+function goBack(){
+  DialogEnd.value = false
+  backPossible.value = false
   cardStore.indexMinusOne()
   router.go(-1)
-  // Hier kannst du router.push oder andere Logik einfügen
 }
 
-const showAnswer = () => {
+function showAnswer() {
   reveal.value = true;
 }
 
-const goHome = () => {
+function goHome() {
   cardStore.resetIndex()
   router.push('/')
 }
@@ -36,19 +50,48 @@ function showEndDialog(){
 }
 
 function nextCard() {
+  backPossible.value = true
   const nextId = cardStore.getNextId()
-  if(nextId === -1){
+  if (nextId === -1) {
     showEndDialog()
-    return;
+    return
   }
-  router.push('/cards/' + cardStore.getNextId())
+  router.push('/cards/' + nextId)
   reveal.value = false
+}
+
+function rateCard(colorIndex: number) {
+  ratingArr.value[cardStore.getCardIndex()] = colorIndex
+  nextCard()
 }
 
 const testDeckName = "Hausfriedensbruch (§ 123 StGB)" //TODO: load deck name
 </script>
 
 <template>
+  <div class="progress-container d-flex flex-column align-center mb-4">
+    <div class="progress-bar d-flex">
+      <div
+        v-for="(color_index, i) in ratingArr"
+        :key="i"
+        :style="{
+          backgroundColor: colorNames[color_index],
+          width: 100 / ratingArr.length + '%',
+          border: i === curCardIndex ? '2px solid'+ theme.current.value.colors.primary : 'none'
+        }"
+      />
+    </div>
+    <div class="mt-1 text-caption text-center">
+      Karte
+      <span v-if="!DialogEnd">
+        {{ curCardIndex + 1 }}
+      </span>
+      <span v-else>
+        {{ cards.length }}
+      </span>
+      / {{ cards.length }}
+    </div>
+  </div>
   <v-responsive class="d-flex justify-center">
     <v-card
       class="mx-auto pa-4"
@@ -118,6 +161,7 @@ const testDeckName = "Hausfriedensbruch (§ 123 StGB)" //TODO: load deck name
       <v-btn
         icon
         style="flex: 0 0 auto;"
+        :disabled="!backPossible"
         @click="goBack"
       >
         <v-icon>mdi-arrow-left</v-icon>
@@ -144,28 +188,28 @@ const testDeckName = "Hausfriedensbruch (§ 123 StGB)" //TODO: load deck name
           <v-btn
             icon
             color="red"
-            @click="nextCard()"
+            @click="rateCard(3)"
           >
             <v-icon>mdi-alpha-x</v-icon>
           </v-btn>
           <v-btn
             icon
             color="orange"
-            @click="nextCard()"
+            @click="rateCard(2)"
           >
             <v-icon>mdi-help</v-icon>
           </v-btn>
           <v-btn
             icon
             color="yellow"
-            @click="nextCard()"
+            @click="rateCard(1)"
           >
             <v-icon>mdi-check</v-icon>
           </v-btn>
           <v-btn
             icon
             color="green"
-            @click="nextCard()"
+            @click="rateCard(0)"
           >
             <v-icon>mdi-check-all</v-icon>
           </v-btn>
@@ -184,9 +228,13 @@ const testDeckName = "Hausfriedensbruch (§ 123 StGB)" //TODO: load deck name
 
   <v-dialog
     v-model="DialogEnd"
-    max-width="344"
+    persistent
+    max-width="90vw"
   >
-    <v-card>
+    <v-card
+      class="mx-auto pa-4"
+      style="max-width: 90vw; max-height: 90vh; overflow-y: auto;"
+    >
       <v-card-title class="text-center d-flex flex-column align-center justify-center">
         <v-icon icon="mdi-party-popper" />
         <span class="mt-2">Geschafft!</span>
@@ -195,6 +243,9 @@ const testDeckName = "Hausfriedensbruch (§ 123 StGB)" //TODO: load deck name
         Herzlichen Glückwunsch du hast alle Karten geschafft
       </v-card-text>
       <v-card-actions>
+        <v-btn @click="goBack">
+          Zurück zur letzten Karte
+        </v-btn>
         <v-btn @click="goHome">
           Zurück zum Dashboard
         </v-btn>
@@ -204,8 +255,14 @@ const testDeckName = "Hausfriedensbruch (§ 123 StGB)" //TODO: load deck name
 </template>
 
 <style scoped lang="sass">
+.progress-container
+  width: 100%
+  max-width: 600px
+  margin: 0 auto
 
-
-
-
+.progress-bar
+  width: 100%
+  height: 14px
+  border-radius: 4px
+  overflow: hidden
 </style>
